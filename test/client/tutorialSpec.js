78b6cca65464fc0eda10a6754fe988681967c3be
athulnath <athulnath.t@gmail.com>
2015-08-20 23:57:15 +0530

    added karma and jasmine test framework environment

---
+++ test/client/tutorialSpec.js
@@ -0,0 +1,16 @@
+describe('tutorial test suit', function() {
+
+	it('should add two numbers', function() {
+		expect(1+3).toBe(4);
+	});
+
+	it('should minus two numbers', function() {
+		expect(3-2).toBe(1);
+	});
+
+	it("check boolean", function() {
+		expect(true).toBe(true);
+	});
+
+
+});
